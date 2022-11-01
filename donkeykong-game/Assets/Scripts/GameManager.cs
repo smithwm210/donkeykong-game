0591b057201cd7b0d81d2ea0e3be1bbbab9123f1
smithwm210 <smithwm210@gmail.com>
2022-11-01 11:03:03 -0400

    Tutorial complete

--- donkeykong-game/Assets/Scripts/GameManager.cs
+++ donkeykong-game/Assets/Scripts/GameManager.cs
@@ -1,26 +1,53 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private int level;
     private int lives;
     private int score;
 
     private void Start() {
+        DontDestroyOnLoad(gameObject);
         NewGame();
-
     }
 
     private void NewGame() {
         lives = 3;
         score = 0;
 
-        // Load level...
+        LoadLevel(1);
+    }
+
+    private void LoadLevel(int index) {
+        level = index;
+
+        Camera camera = Camera.main;
+
+        // screen goes black for 1 sec
+        if (camera != null) {
+            camera.cullingMask = 0;
+        }
+        Invoke(nameof(LoadScene), 1f);
+    }
+
+    private void LoadScene() {
+        SceneManager.LoadScene(level);
     }
 
     public void LevelComplete() {
         score += 1000;
 
-        // Load next level
+        int nextLevel = level + 1;
+
+        // check that next level exists
+        if (nextLevel < SceneManager.sceneCountInBuildSettings) {
+            LoadLevel(nextLevel);
+        }
+        else {
+            LoadLevel(1);
+        }
+        
     }
 
     public void LevelFailed() {
@@ -29,7 +56,7 @@ public class GameManager : MonoBehaviour
             NewGame();
         }
         else {
-            // reload current level
+            LoadLevel(level);
         }
 
     }
