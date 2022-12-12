70a113d41b0f9e48b0edde9a1e3a4aa0ebe035b9
smithwm210 <smithwm210@gmail.com>
2022-12-11 23:06:27 -0500

    zombie animations

---
+++ donkeykong-game/Assets/Scripts/Zombie.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+
+    [SerializeField] bool grounded = false;
+
+    [SerializeField] Transform groundCheckCollider;
+    [SerializeField] LayerMask groundLayer;
+
+    const float groundCheckRadius = 0.2f;
+
+    void FixedUpdate()
+    {
+        GroundCheck();
+    }
+
+    private void GroundCheck()
+    {
+        grounded = false;
+
+        // see if groundcheck object is colliding with other
+        // 2D colliders in the "Ground" layer
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckCollider.position, groundCheckRadius, groundLayer);
+        if (colliders.Length > 0)
+            grounded = true;
+    }
+}
